00c830e42997fe2f357a9a9cf56dc5d1003bf14e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-01 00:34:41 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_actors_to_characters.rb
+++ db/migrate/006_add_actors_to_characters.rb
@@ -1,8 +1,5 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
-    create_table :characters do |t|
-      t.string :name
-      t.integer :show_id
-    end
+   add_column :actors 
   end
 end
